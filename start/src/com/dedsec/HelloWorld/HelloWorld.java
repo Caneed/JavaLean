0f3795aca113c56fd3ae16b4a49c7ce37a3d6e14
dedsec <1115659949@qq.com>
2023-11-02 22:21:46 +0800

    java learn start

---
+++ start/src/com/dedsec/HelloWorld/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.dedsec.HelloWorld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+//        开始
+        System.out.println("hello,world");
+    }
+}
---
+++ start/src/com/dedsec/HelloWorld/Literal.java
@@ -0,0 +1,15 @@
+package com.dedsec.HelloWorld;
+
+public class Literal {
+    //    整数，小数，字符，字符串，布尔值，空值
+    public static void main(String[] args) {
+        int a = 1;
+        boolean b = false;
+        double c = 2.4;
+        String d = "aaa";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
